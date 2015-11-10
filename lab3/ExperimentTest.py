efe3418ce07f4d9ca42f3814691baa24ea960f34
Vedha Raman <vedhapriya.raman@utexas.edu>
2015-11-09 18:43:57 -0600

    Update ExperimentTest.py

--- lab3/ExperimentTest.py
+++ lab3/ExperimentTest.py
@@ -21,7 +21,7 @@ class MyTestCase(unittest.TestCase):
 
   def test_t5(self):
     r1 = Experiment.largest([3,-2,3,-1,4]) 
-    self.assertEqual(r1, 4)
+    self.assertEqual(r1, 5)
 
   def test_t6(self):
     r1 = Experiment.largest([-3,-2,-3,-1,-4]) 
